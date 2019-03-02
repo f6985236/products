908a3d66925a5ba91ab36341c667e12b99504fb1
kiwee <f6985236@gmail.com>
2019-03-02 15:56:01 +0800

    add read file

--- products.py
+++ products.py
@@ -1,9 +1,26 @@
+#---------------------------先讀取檔案
+products = []
+with open('products.csv', 'r', encoding= 'utf-8') as f :
+	for line in f :
+		if '商品,價格' in line:#這個步驟是要讓商品、價格這種定義不會也出現在我們清單中的一部分
+			continue#繼續=進行下一回迴圈。 break是等於直接跳出迴圈，不一樣
+        
+		name,price = line.strip().split(',')#直接將name, price對應到逗號分割的兩者＆strip是用來去除空行\n符號顯示
+		 #只要遇到逗號就分割，等於excel裡頭直接分行
+		products.append([name,price])
+print(products)
+
+
+
+
+
 #while loop比較適合在不確定要執行幾次的狀態下使用，比起forloop
 
 #問使用者買的商品名稱、價格，然後把它輸入的東西加入一個清單
 #建立二維度清單，清單中的清單，有名字後價格
+ 
 
-products = [] 
+#--------------------------讓使用者輸入
 while True:
 	name = input('請輸入商品名稱')
 	if name == 'q':
@@ -26,17 +43,17 @@ print(products)
 
 #建立二維度清單，清單中的清單，有名字後價格
 
-#--------------
+#--------------印出所有購買紀錄
 
 for p in products: #Ｐ是小清單！把小清單從大清單一個個拿出來
 	print(p[0],'的價格是',p[1])#印出小火車的第零格＝name，和價格
 
  #---創建檔案！
-
+#---------寫入檔案！------------
 with open('products.csv', 'w', encoding= 'utf-8') as f: #w是寫入，創造檔案,存成csv就是存成excel
 	f.write('商品名稱,價格\n') #在數字上面寫上一層簡介，注意空格跳行符號
 	for p in products:
-			f.write(p[0] + ',' + str(p[1])+ '\n')  #逗點可以幫助excel分隔，把名稱和價格分開
+			f.write(p[0] + ',' + str(p[1])+ '\n')  #逗點可以幫助excel分隔，把名稱和價格分開，打開excel的時候選擇Delimiters:camma
 			#任何程式有open就有close,python是用with open 來自動close
-			#str(p[1]) 字串只能與字串家
+			#str(p[1]) 字串只能與字串相加
 
